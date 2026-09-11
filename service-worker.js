2f918a550f821ccd7741813e3ff122b8909fdcb5
20op4letzebuerg-rgb <20op4letzebuerg@gmail.com>
2026-09-11 21:34:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "growlog-v1";
+const CACHE_NAME = "growlog-v2";
 
 const APP_FILES = [
   "./",
@@ -8,9 +8,7 @@ const APP_FILES = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_FILES);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
   );
 
   self.skipWaiting();
@@ -18,22 +16,40 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
+    caches.keys().then(names =>
+      Promise.all(
+        names
           .filter(name => name !== CACHE_NAME)
           .map(name => caches.delete(name))
-      );
-    })
+      )
+    )
   );
 
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
     })
   );
 });
